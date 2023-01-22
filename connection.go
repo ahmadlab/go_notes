e67f2ffa5107dbf692f2bccef807de364873bbd9
Ahmad Juma <ahmadnorin@gmail.com>
2023-01-22 21:51:37 +0700

    Create connection.go

---
+++ connection.go
@@ -0,0 +1,37 @@
+package Configs
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+var (
+	Dbconn *gorm.DB
+)
+
+func DBInit() *gorm.DB {
+
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println(r)
+		}
+	}()
+
+	dsn := "host=localhost user=liramedika password=liramedika dbname=danadesa port=5432 sslmode=disable TimeZone=Asia/Taipei"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("koneksi kedatabase berhasil")
+	Dbconn = db
+	//return Dbconn
+	return Dbconn
+
+}
